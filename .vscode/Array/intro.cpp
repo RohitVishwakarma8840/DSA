d703d78e8ba3fc29e6b960c62598b489d7ed3a0b
Rohit Vishwakarma <rohit8840@gmail.com>
2024-01-27 23:43:43 +0530

    adding more files

---
+++ .vscode/Array/intro.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+// int arr[5]; // array declaration 
+
+// int arr[5]={1,2,3,4,5};
+// for(int i=0;i<5;i++){
+
+//     cout<<arr[i]<<" ";
+// }
+
+// int arr[]={1,2,3,4,5,6};
+// for(int i=0;i<6;i=i+1){
+//     cout<<arr[i]<<" ";
+// }
+
+int arr[]={1,2,3};
+for(int i=0;i<6;i=i+1){
+    cout<<arr[i]<<" ";
+}
+
+    return 0;
+}
