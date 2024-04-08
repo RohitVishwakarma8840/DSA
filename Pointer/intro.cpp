10548eb5355b81db0af2a888d7dbbcc256ed2e44
Rohit Vishwakarma <rohit8840@gmail.com>
2024-04-08 14:08:38 +0530

    code insertion

---
+++ Pointer/intro.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//  Print the address 
+int a=10;
+cout<<&a<<endl;
+
+// Using pointer to print the address 
+int *ptr=&a;
+cout<<ptr<<endl;
+
+float m=3.5;
+float *ptr1=&m;
+cout<<ptr1<<endl;
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
