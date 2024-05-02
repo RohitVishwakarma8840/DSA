624b45686b9a899d6481472c097023ded2e30cd0
Rohit Vishwakarma <rohit8840@gmail.com>
2024-05-02 09:27:08 +0530

    adding code

---
+++ RECURSION/intro.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void fun(int n ){
+
+if(n==0){
+  cout<<"Happy Birthday"<<endl;
+   return ;
+}
+
+cout<<n<<" Days left for the Birthday"<<endl;
+ fun(n-1);
+
+}
+
+
+
+int main(){
+
+int n=3;
+// fun(n);
+
+fun(7);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RECURSION/second.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void print(int n){
+
+if(n==1){
+    cout<<"1" ;
+    return ;
+}
+
+cout<<n<<endl;
+print(n-1);
+
+}
+
+int main(){
+
+int n=5;
+
+print(n);
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
