418a17aa69af1202c68abd343f6ffa65bc6e5acc
Rohit Vishwakarma <rohit8840@gmail.com>
2024-06-19 10:53:03 +0530

    adding code

---
+++ RECURSION_ARRAY/for_loop_iterative_approach.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void print(int arr[], int index){
+
+    cout<<arr[index]<<" ";
+}
+
+
+
+int main(){
+
+int arr[]={4,3,5,6,7};
+
+for(int i=0;i<5;i++)
+print(arr,i);
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
