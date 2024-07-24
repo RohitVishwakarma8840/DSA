64db69307fccfaa5060a687a7f589846b542d69a
Rohit Vishwakarma <rohit8840@gmail.com>
2024-07-24 23:55:57 +0530

    adding rat que

---
+++ .vscode/RECURSION_QUESTION/Rat_in_a_Maze.cpp
@@ -0,0 +1,48 @@
+ public:
+  
+  int row[4]={-1,1,0,0};
+  int col[4]={0,0,-1,1};
+  
+   string dir="UDLR";
+  
+  bool valid(int i , int j , int n){
+      return i>=0  && j>=0 && i<n && j<n;
+  }
+  
+  void Total(vector<vector<int>>&matrix , int i, int j, int n , string &path , vector<string>&ans , vector<vector<int>>&visited){
+      
+      if(i==n-1 && j==n-1){
+          ans.push_back(path);
+          return ;
+      }
+      visited[i][j]=1;
+      
+      for(int k=0;k<4;k++){
+          
+          if(valid(i+row[k],j+col[k],n) && matrix[i+row[k][j+col[k]]] && !visited[i+row[k]][j+col[k]]){
+              path.push_back(dir[k]);
+              Total(matrix , i+row[k],j+col[k],n,path , ans , visited)
+          }
+          
+      }
+      
+  }
+  
+  
+  
+  
+    vector<string> findPath(vector<vector<int>> &m,int n) {
+        
+        vector<vector<bool>>visited(n,vector<bool>(n,0));
+        vector<string>ans;
+        string path;
+        
+        if(m[0][0]==0 || m[n-1][n-1]==0)
+        return ans;
+        
+        Total(m,0,0,n,path,ans,visited);
+        
+        
+        
+    }
+};
