79302f5e2f76dae812eb60a38c8691a6d83ee465
Rohit Vishwakarma <rohit8840@gmail.com>
2024-08-15 15:28:52 +0530

    adding xnode

---
+++ Linked List/Delete_LinkedList/At_Particular_Position_Deletion.cpp
@@ -0,0 +1,30 @@
+Node* deleteNode(Node *head,int x)
+{
+    
+    
+    if(x==1){
+        Node *temp;
+        head=head->next;
+        delete temp;
+        
+        return head;
+    }
+        
+        x--;
+        Node *curr=head;
+        Node *prev=NULL;
+        
+        while(x--){
+            
+            prev=curr;
+            curr=curr->next;
+            
+        }
+        
+        
+        prev->next=curr->next;
+        delete curr;
+        
+        
+        return head;
+}
