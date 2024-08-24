98f5f598394fe24a17bee85d5be92e069067888a
Rohit <rohit8840185@gmail.com>
2024-08-24 06:21:36 -0700

    reversell

--- Linked List 2/Recursion_Reverse_LinkedLIst.cpp
+++ Linked List 2/Recursion_Reverse_LinkedLIst.cpp
@@ -1,7 +1,31 @@
-#include<iostream>
-using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   
+  ListNode *Reverse(ListNode *curr, ListNode *prev){
+    if(curr==NULL)
+    return prev;
 
-int main(){
-    cout<<"hello world "<<endl;
-    return 0;
-}
+    ListNode *fut = curr->next;
+      curr->next = prev;
+      return Reverse(fut,curr);
+
+
+
+  }
+
+
+    ListNode* reverseList(ListNode* head) {
+        return Reverse(head,NULL);
+        
+    }
+};
