c84072aca7a14f047a72a25c5b36389ea7bb0215
Rohit <rohit8840185@gmail.com>
2024-09-01 20:04:44 +0530

    adding dlreww

---
+++ Doubly Linked List/Deletion_at_any_point.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int value;
+    Node *next=NULL;
+    Node *prev=NULL;
+
+    Node(int val){
+        value=val;
+        next,prev=NULL;
+    }
+
+
+};
+
+// 1 2 3 4 
+
+ Node *CreateDLL(int arr[],int index , int size ,Node *back){
+
+// Base Case 
+   if(index==size)
+   return NULL;
+
+   Node *temp= new Node(arr[index]);
+    temp->prev=back;
+    temp->next = CreateDLL(arr,index+1, 5,temp);
+     
+     return temp;
+
+ }
+
+int main(){
+
+ Node *head=NULL;
+
+// Recursion Doubly Linked List 
+
+  int arr[]={1,2,3,4,5};
+
+
+  // Calling the Recursive function 
+    head= CreateDLL(arr,0,5,NULL);
+ 
+
+  Node *trav=head;
+  while(trav){
+    cout<<trav->value<<" ";
+    trav=trav->next;
+  }
+
+
+    return 0;
+}
