4a06fe313400472c7d6060f50c2774859ce8d082
Rohit <rohit8840185@gmail.com>
2024-09-25 13:50:25 +0530

    adding maximal rectangle

---
+++ Stack_questions_Part_2/85_Leetcode_Largest_Maximal_Rectangle.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+int largestRectangleArea(vector<int>& heights) {
+        int ans=0;
+        int index;
+        stack<int>st;
+        int n = heights.size();
+
+        for(int i=0;i<n;i++){
+
+            while(!st.empty() && heights[st.top()]>heights[i]){
+          index = st.top();
+          st.pop();
+          if(!st.empty())
+          ans = max(ans , heights[index]*(i-st.top()-1));
+          else
+          ans = max(ans, heights[index]*i);
+            }
+
+       st.push(i);
+
+        }
+
+     while(!st.empty()){
+    
+        index = st.top();
+          st.pop();
+          if(!st.empty())
+          ans = max(ans , heights[index]*(n-st.top()-1));
+          else
+          ans = max(ans, heights[index]*n);
+
+     }
+
+       return ans;
+
+
+}
+
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int ans=0; 
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<int>height(col,0);
+
+        for(int i=0; i<row;i++){
+          for(int j=0; j<col ; j++){ 
+            if(matrix[i][j]=='0')
+            height[j]=0;
+            else
+            height[j]++;
+          }
+          ans = max(ans, largestRectangleArea(height));
+        }
+        
+        return ans;
+    }
+};
