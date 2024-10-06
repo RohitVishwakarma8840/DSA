8ec318915bbcd0ef31286968bf03e167a2ea18a5
Rohit <rohit8840185@gmail.com>
2024-10-06 16:56:35 +0530

    height_binary_tree

---
+++ Trees Questions/Height_Of_Binary_Tree_GFG.cpp
@@ -0,0 +1,26 @@
+// struct Node
+// {
+//     int data;
+//     struct Node* left;
+//     struct Node* right;
+    
+//     Node(int x){
+//         data = x;
+//         left = right = NULL;
+//     }
+// };
+
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        
+        if(node==NULL)
+        return 0;
+      
+        
+    return (1+max(height(node->left),height(node->right)));
+
+        
+    }
+};
