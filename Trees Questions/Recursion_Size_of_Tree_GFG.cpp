20c9850abf157ce84eebb84bbccea355b97c88c3
Rohit <rohit8840185@gmail.com>
2024-10-06 10:47:13 +0530

    getSize recursion

---
+++ Trees Questions/Recursion_Size_of_Tree_GFG.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int getSize(Node* node) {
+        
+        if(node==NULL)
+        return 0;
+        
+        
+        return (1+ getSize(node->left) + getSize(node->right));
+        
+        
+    }
+};
