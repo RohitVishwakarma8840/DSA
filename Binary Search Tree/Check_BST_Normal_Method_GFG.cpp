f4225c967f827090de656daabe0ceebca20a456a
Rohit <rohit8840185@gmail.com>
2024-11-01 19:44:44 +0530

    adding check bst

---
+++ Binary Search Tree/Check_BST_Normal_Method_GFG.cpp
@@ -0,0 +1,28 @@
+ class Solution {
+  public:
+  
+  void BST(int arr[],int &index,int lower,int upper,int N){
+      
+      if(index==N || arr[index]<lower || arr[index]>upper)
+      return;
+      
+     int value = arr[index++]; 
+      
+      // left 
+      BST(arr,index,lower,value,N);
+      
+      // Right
+      BST(arr,index,value,upper,N);
+      
+  }
+  
+  
+    int canRepresentBST(int arr[], int N) {
+        // code here
+        
+        int index = 0;
+        BST(arr,index,INT_MIN,INT_MAX,N);
+        
+        return index==N;
+    }
+};
