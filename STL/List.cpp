793bf0b95140afe8eaafbe0ccdeff81d08f8079f
Rohit <rohit8840185@gmail.com>
2024-12-25 16:54:03 +0530

    stl list value

--- STL/List.cpp
+++ STL/List.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
 
+ list<int>l;
+
+ l.push_back(20);
+ l.push_back(30);
+ l.push_back(40);
+ l.push_back(50);
+ l.push_front(15);
+
+ cout<<l.front()<<" "<<l.back()<<endl;
+ cout<<l.size()<<endl;
+
+
+ for(auto it = l.begin(); it!=l.end(); it++){
+
+    cout<<*it<<" ";
+ }
 
 
 
