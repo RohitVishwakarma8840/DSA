954f8c8bee3fa0627236e522e0af2f254f477e4e
Rohit <rohit8840185@gmail.com>
2024-12-25 17:23:09 +0530

    set stl

---
+++ STL/set.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // only unique val will be stored 
+    // store value in sorted order 
+    // insertion , deletion , search have o(logn) complexity 
+    // generally implemented using red black tree
+    // we can sort it in descending using 
+    // greater<type> operator.
+
+ set<int>s;
+
+ s.insert(10);
+ s.insert(20);
+ s.insert(30);
+ s.insert(10);  // duplicate values cannot be inserted 
+ s.insert(40);
+
+
+ for(auto it = s.begin(); it!=s.end(); it++){
+
+   cout<< *it<<" ";
+     
+ }
+
+    return 0;
+}
