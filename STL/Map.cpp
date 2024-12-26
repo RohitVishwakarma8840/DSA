7916bc09fe266ee903ff9865b02f72e2c35fea62
Rohit <rohit8840185@gmail.com>
2024-12-26 18:21:56 +0530

    adding map hashmap

---
+++ STL/Map.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+// creating a map
+map<int,int>m;
+
+// Map stores in key-value pair .
+// key should be unique .
+// implemented using AVL Tree or red black tree.
+// O(logn)  search , delete , update .
+
+
+ m.insert(make_pair(20,30));
+ m.insert(make_pair(30,310));
+ m.insert(make_pair(40,230));
+ m.insert(make_pair(20,230));
+ m.insert(make_pair(50,30));
+
+
+
+
+
+    return 0;
+}
---
+++ STL/Unorder_multiset.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+ unordered_multiset<int>s;
+
+
+  // no order it follows means it is unordered 
+  // O(1) Time complexity 
+  // hashing implementation 
+  // stores duplicate element 
+  
+ s.insert(10);
+ s.insert(20);
+ s.insert(20);
+ s.insert(30);
+ s.insert(40);
+ s.insert(50);   
+ s.insert(90);   
+ s.insert(100);   
+
+
+ for(auto it = s.begin() ; it!= s.end(); it++){
+    cout<< *it<< " ";
+ }
+
+
+    return 0;
+}
