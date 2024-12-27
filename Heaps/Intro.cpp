0d69e21f219dee9221317a921f48d214dd50b3f9
Rohit <rohit8840185@gmail.com>
2024-12-27 15:51:51 +0530

    adding heap code

--- Heaps/Intro.cpp
+++ Heaps/Intro.cpp
@@ -21,9 +21,36 @@ class MaxHeap{
   // insert into the heap 
   void insert(int value){
 
+    // heap size is available or not 
+    if(size==total_size){
+        cout<<"Heap overflow"<<endl;
+        return;
+    }
+
+ // if space available then insert 
+  arr[size] = value;
+  int index = size;
+  size++;
+
+  while(index>0 && arr[(index-1)/2]<arr[index]) 
+  {
+    swap(arr[index],arr[(index-1)/2]);
+    index = (index-1)/2;
+  };
+
+  cout<<arr[index]<< " is inserted into the heap"<<endl;
+
 
   }
 
+  void print(){
+
+     for(int i=0 ; i<size; i++){
+        cout<<arr[i]<<" ";
+     }
+     cout<<endl;
+  }
+ 
 };
 
 int main(){
@@ -32,6 +59,7 @@ int main(){
  H1.insert(4);
  H1.insert(6);
  H1.insert(11);
+ H1.print();
 
 
 
