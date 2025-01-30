d0985ef6c51e07c582cfe904ee36da7643986db1
Rohit <rohit8840185@gmail.com>
2025-01-30 13:46:40 +0530

    Adding code bipartite

---
+++ Graph/Graph_Questions/Bipartite_Graph_GFG.cpp
@@ -0,0 +1,43 @@
+Solution {
+  public:
+    bool isBipartite(vector<vector<int>>& adj) {
+        
+        int V= adj.size();
+        
+        vector<int>color( V,-1);
+        queue<int>q;
+        
+        q.push(0);
+        color[0] =0;
+        
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            for(int j=0; j<adj[node].size() ; j++){
+                
+                // color not assigned to them 
+                if(color[adj[node][j]]==-1){
+                    color[adj[node][j]] = (color[node]+1)%2;
+                    q.push(adj[node][j]);
+                }
+                else{
+                // color is not 
+                if(color[node]==color[adj[node][j]])
+                return 0;     
+                }
+            
+            }
+            
+            
+            
+        }
+        
+        return 1;
+        
+    
+        
+        
+    }
+};
