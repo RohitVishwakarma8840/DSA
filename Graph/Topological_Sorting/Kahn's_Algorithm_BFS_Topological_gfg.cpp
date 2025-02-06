de36d92d3ada2f3ddf4e8e22cf97f027ee8772b9
Rohit <rohit8840185@gmail.com>
2025-02-06 12:38:11 +0530

    Adding kahn's algo

---
+++ Graph/Topological_Sorting/Kahn's_Algorithm_BFS_Topological_gfg.cpp
@@ -0,0 +1,52 @@
+class Solution {
+  public:
+  
+    
+    
+    vector<int> topologicalSort(vector<vector<int>>& adj) {
+        
+        int V = adj.size();
+        vector<int>ans;
+        
+        // we will find the dfs 
+        vector<int>InDeg(V,0);
+
+        
+        for(int i=0;i<V;i++){
+            for(int j=0; j<adj[i].size(); j++){
+                InDeg[adj[i][j]]++;
+            }
+        }
+        
+        
+        // push all the elements in the stack 
+        queue<int>q;
+      for(int i=0; i<V;i++)
+      if(!InDeg[i])
+      q.push(i);
+      
+      while(!q.empty()){
+          
+         int node = q.front();
+         q.pop();
+         ans.push_back(node);
+         
+         // push the neighbours 
+         for(int j=0; j<adj[node].size(); j++){
+             InDeg[adj[node][j]]--;
+             if(InDeg[adj[node][j]]==0)
+             q.push(adj[node][j]);
+             
+             
+         }
+          
+          
+          
+      }
+
+        
+        
+        return ans;
+        
+    }
+};
