970c40285bfedfc379d207a2a9494e23472e0a70
Rohit <rohit8840185@gmail.com>
2025-02-19 17:13:27 +0530

    adding mmdp

---
+++ Dynamic_Programming/Memorization_Dp.cpp
@@ -0,0 +1,33 @@
+class  Solution {
+    public:
+    
+    int find(int n,vector<int>dp){
+        
+        // Base Case
+        if(n<=1)
+        return n;
+        
+        if(dp[n]!=-1)
+        return dp[n];
+        
+        
+        dp[n] = find(n-1,dp)+find(n-2,dp);
+        return dp[n];
+        
+        
+        
+    }
+    
+    
+    
+      int nthFibonacci(int n) {
+          // code here
+          
+          vector<int>dp(n+1,-1);
+       
+          return find(n,dp);
+         
+          
+          
+      }
+  };
